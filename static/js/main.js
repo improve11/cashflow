8275e39f08d3588c1f8813ab676796bd725b3289
Vlad <142506318+improve11@users.noreply.github.com>
2025-03-06 04:12:42 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,28 @@
+// Динамическое обновление категорий
+function updateCategories() {
+    const typeId = $("#id_type").val();
+    if (typeId) {
+        $.get(`/load-categories/?type_id=${typeId}`, function(data) {
+            $("#id_category").html(data);
+        });
+    }
+}
 
+// Динамическое обновление подкатегорий
+function updateSubcategories() {
+    const categoryId = $("#id_category").val();
+    if (categoryId) {
+        $.get(`/load-subcategories/?category_id=${categoryId}`, function(data) {
+            $("#id_subcategory").html(data);
+        });
+    }
+}
+
+$(document).ready(function(){
+    $("#id_type").change(updateCategories);
+    $("#id_category").change(updateSubcategories);
+    
+    // Инициализация при загрузке
+    updateCategories();
+    updateSubcategories();
+});
